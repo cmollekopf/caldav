d582ace82c57c2d8c31d7e90914c1ef9946a2bf8
Mario Lubenka <mario.lubenka@googlemail.com>
2020-02-11 22:37:50 +0100

    test: split parsers_test.dart into single tests
    Tests from this file were moved into other test files that lie in the new folder structure

---
+++ test/src/webdav/parser/response_test.dart
@@ -0,0 +1,23 @@
+import 'package:caldav/src/webdav/parser/response.dart';
+import 'package:test/test.dart';
+import '../../../_fixtures/caldav_data.dart' as data;
+
+import 'package:xml/xml.dart' as xml;
+
+void main() {
+  test('parse sample response', () async {
+    var processor = new ResponseParser();
+    var node = xml.parse(data.nextCloudCurrentUser);
+    var result = processor.parse(node);
+
+    // has three results with href
+    expect(result.length, 3);
+    expect(result.elementAt(0).getHref(), '/remote.php/caldav/');
+    expect(result.elementAt(1).getHref(), '/remote.php/caldav/principals/');
+    expect(result.elementAt(2).getHref(), '/remote.php/caldav/calendars/');
+
+    // has propstat
+    expect(result.elementAt(0).propStats.length, 1);
+  });
+
+}
