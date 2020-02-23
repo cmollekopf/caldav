4820261c7ec327a276a1d8d65f65571c514fd44e
Mario Lubenka <mario.lubenka@googlemail.com>
2020-02-23 20:12:38 +0100

    refactor: CaldDavElement should inherit from WebDavElement

--- lib/src/caldav/caldav_element.dart
+++ lib/src/caldav/caldav_element.dart
@@ -1,13 +1,12 @@
-import '../core/xmlelement.dart';
+import '../webdav/webdav_element.dart';
 
 /// Namespace of CalDAV elements
 const calDavNamespace = 'urn:ietf:params:xml:ns:caldav';
 
 /// Base class for CalDAV elements
-class CalDavElement extends XmlElement {
+class CalDavElement extends WebDavElement {
   /// Constructor for CalDAV elements
-  CalDavElement(name) {
-    this.name = name;
+  CalDavElement(name) : super(name) {
     namespace = calDavNamespace;
   }
 }
